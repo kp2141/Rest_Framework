21e4794893298c70bac689543cdb47c7c475bbb7
kp2141 <kp2141krp@gmail.com>
2018-06-03 12:19:19 +0530

    vreated first api view

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
 # Create your views here.
+
+class HelloApiView(APIView):
+
+    def get(self,request,format=None):
+
+        an_apiview = [
+            'Uses HTTP methods as function (get,post,patch,put,delete)',
+            'It is similar to a traditional Django view'
+        ]
+
+        return Response({'message':'Hello!','an_apiview':an_apiview})
